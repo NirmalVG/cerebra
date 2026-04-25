efb28d56c5a4fdb2670d290ae36a601833692fdb
NIRMAL V G <Nirmalvg97@gmail.com>
2026-04-25 13:07:12 +0530

    fixes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   compress: true,
-  optimizeFonts: true,
   headers: async () => [
     {
       source: "/:path*",
